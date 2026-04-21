074873cbbd32167b526d5636161768d9b381ef5a
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-04-21 20:46:06 +0100

    feat: add EKS module

---
+++ infra/modules/eks/variables.tf
@@ -0,0 +1,34 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
+variable "instance_types" {
+  description = "List of instance types for the node group"
+  type        = list(string)
+}
