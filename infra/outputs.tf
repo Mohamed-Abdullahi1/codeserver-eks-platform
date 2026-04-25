634584fa59682a01513eb4afbea78c7edaa5af48
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-04-25 22:40:17 +0100

    feat: add Route 53 outputs for DNS configuration

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -21,3 +21,11 @@ output "cluster_endpoint" {
 output "cluster_arn" {
   value = module.eks.cluster_arn
 }
+
+output "route53_zone_id" {
+  value = module.route53.zone_id
+}
+
+output "route53_name_servers" {
+  value = module.route53.name_servers
+}
