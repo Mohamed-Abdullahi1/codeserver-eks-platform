e63107b90ea132359d41ffe3b8381456da5a1899
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-04-27 12:43:16 +0100

    style: terraform fmt

--- infra/modules/external-dns-irsa/main.tf
+++ infra/modules/external-dns-irsa/main.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "external_dns" {
   }
 
   statement {
-    effect  = "Allow"
+    effect = "Allow"
     actions = [
       "route53:ListHostedZones",
       "route53:ListResourceRecordSets",
