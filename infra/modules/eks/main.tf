d21aceaeeca190871ddb26a315533b6d3361f77f
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-04-27 12:09:19 +0100

    feat: add OIDC provider to EKS module for IRSA support

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -83,3 +83,17 @@ resource "aws_eks_node_group" "this" {
     aws_iam_role_policy_attachment.eks_ecr_policy,
   ]
 }
+
+data "tls_certificate" "eks_oidc" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  client_id_list = ["sts.amazonaws.com"]
+  
+  thumbprint_list = [
+    data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint
+  ]
+}
