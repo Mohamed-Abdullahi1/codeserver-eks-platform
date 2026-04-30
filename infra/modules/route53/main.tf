b9d929e71ccaa3213959a5b962d3efc61006e92f
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-04-30 18:25:29 +0100

    refactor: use Route53 hosted zone data source

--- infra/modules/route53/main.tf
+++ infra/modules/route53/main.tf
@@ -1,7 +1,4 @@
-resource "aws_route53_zone" "this" {
-  name = var.domain_name
-
-  lifecycle {
-    prevent_destroy = true
-  }
+data "aws_route53_zone" "this" {
+  name         = var.domain_name
+  private_zone = false
 }
