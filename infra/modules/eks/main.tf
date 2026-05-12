193d2f49fc7f0368cde9b28f2a2f699605975ac0
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-05-12 12:38:20 +0100

    feat: configure EKS access entries and API auth mode

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -53,6 +53,11 @@ resource "aws_eks_cluster" "this" {
   version  = var.kubernetes_version
   role_arn = aws_iam_role.eks_cluster_role.arn
 
+
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
   vpc_config {
     subnet_ids              = var.private_subnet_ids
     endpoint_private_access = true
@@ -62,6 +67,22 @@ resource "aws_eks_cluster" "this" {
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
+}
+
+resource "aws_eks_access_entry" "local_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = var.local_admin_arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "local_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = aws_eks_access_entry.local_admin.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
 }
 
 resource "aws_eks_node_group" "this" {
