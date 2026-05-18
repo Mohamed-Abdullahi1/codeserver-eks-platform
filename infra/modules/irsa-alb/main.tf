bba9c61acd013eb027579723fe55f2b7f5d2a32d
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-05-18 11:19:24 +0100

    fix: remove duplicate ALB IRSA output

--- infra/modules/irsa-alb/main.tf
+++ infra/modules/irsa-alb/main.tf
@@ -46,6 +46,3 @@ resource "aws_iam_role_policy_attachment" "alb_controller" {
   policy_arn = aws_iam_policy.alb_controller.arn
 }
 
-output "role_arn" {
-  value = aws_iam_role.alb_controller.arn
-}
