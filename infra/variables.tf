9cb51b0cc74118225cdb0def36aa705dcb31bc66
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-06-03 11:58:11 +0100

    refactor: move static Terraform inputs into defaults

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,17 +1,25 @@
 variable "vpc_cidr" {
-  type = string
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  type = list(string)
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  type = list(string)
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "availability_zones" {
-  type = list(string)
+  description = "Availability zones used by the platform"
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "argocd_namespace" {
@@ -29,6 +37,7 @@ variable "argocd_chart_version" {
 variable "domain_name" {
   description = "Root domain for Route 53 hosted zone"
   type        = string
+  default     = "codeserver.moabdullahi.uk"
 }
 
 variable "local_admin_arn" {
