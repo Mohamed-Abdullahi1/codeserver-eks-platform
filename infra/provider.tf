40853786cd7ed8fc783e383e278a703d79aa22e0
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-06-10 11:01:49 +0100

    chore: pin terraform provider versions

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 6.42.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "= 3.1.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 3.1.1"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
